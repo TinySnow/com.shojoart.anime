a218e5d780ad7d525dcb865e61b0a55ad3328bee
TinySnow <i.little.snow.yangyi@gmail.com>
2020-03-22 21:08:26 +0800

    新建MainMenu，DrawerLayout，ToolBar，定义图标。未引入MainActivity,里面的方法已经重写。

---
+++ app/src/main/java/com/shojoart/anime/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.shojoart.anime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_activity_left_drawerlayout.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        return true
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
