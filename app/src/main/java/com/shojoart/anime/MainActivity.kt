f7cc37066ea2e1567885cc640026f20ea07289a8
TinySnow <i.little.snow.yangyi@gmail.com>
2020-03-24 16:56:41 +0800

    删去冗余的东西，试图将toolbar引入但未成功。

--- app/src/main/java/com/shojoart/anime/MainActivity.kt
+++ app/src/main/java/com/shojoart/anime/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.shojoart.anime
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.main_activity_left_drawerlayout.*
+import kotlinx.android.synthetic.main.main_activity_toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setSupportActionBar(main_activity_toolbar)
+//        val actionbar : androidx.appcompat.app.ActionBar? = supportActionBar
+//        if (actionbar != null){
+//            actionbar.setDisplayHomeAsUpEnabled(true)
+//            actionbar.setHomeAsUpIndicator(R.drawable.ic_menu)
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
         return true
     }
 
