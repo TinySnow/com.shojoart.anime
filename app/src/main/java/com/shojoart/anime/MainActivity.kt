c4321570ffdc61a796d0f935a0275200a48d7ec5
TinySnow <i.little.snow.yangyi@gmail.com>
2020-03-29 15:13:57 +0800

    修正文件，引入toolbar，menu，导航栏除点击事件基本完成

--- app/src/main/java/com/shojoart/anime/MainActivity.kt
+++ app/src/main/java/com/shojoart/anime/MainActivity.kt
@@ -1,29 +1,29 @@
 package com.shojoart.anime
 
-import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.widget.Toolbar
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.main_activity_left_drawerlayout.*
-import kotlinx.android.synthetic.main.main_activity_toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(main_activity_toolbar)
-//        val actionbar : androidx.appcompat.app.ActionBar? = supportActionBar
-//        if (actionbar != null){
-//            actionbar.setDisplayHomeAsUpEnabled(true)
-//            actionbar.setHomeAsUpIndicator(R.drawable.ic_menu)
-//        }
+        setSupportActionBar(main_acti_toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        return true
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onStart() {
